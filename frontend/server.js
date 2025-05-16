f49399990d5d835e929d6ed23c14a38dc5130e19
Lucas Etteldorf <lucas@etteldorf.biz>
2025-05-16 12:48:33 +0200

    Adjust frontend project + Docker config

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,12 @@
-import express from "express";
-import path from "path";
-
-const __dirname = import.meta.dirname;
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
+const __dirname = import.meta.dirname;
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>SmartStore</h1>
+    </>
+  );
+};
+
+export default App;
