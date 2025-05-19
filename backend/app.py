3d343460e53f02f395aac787cdc87d12fe20f6a7
Max Finkenbeiner <runlaysnetwork@gmail.com>
2025-05-19 13:43:06 +0200

    MQTT Sensor Topic DEMO

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,15 @@
 import redis
 from fastapi import FastAPI
 import uvicorn
+from mqtt import init_mqtt
+
+
 
 app = FastAPI()
 
 # Connect to Redis
 # Use the service name ('redis') if using Docker Compose, or the appropriate host
-
-
+init_mqtt()
 
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
@@ -26,3 +28,19 @@ async def root():
         "message": "Success",
         "redis_value": stored_value.decode('utf-8') if stored_value else None
     }
+
+@app.get("/api/history")
+async def get_history():
+    # Fetch data from Redis
+    key = "user:1"
+    stored_value = redis_client.get(key)
+    
+    if stored_value:
+        return {
+            "message": "Data retrieved successfully",
+            "redis_value": stored_value.decode('utf-8')
+        }
+    else:
+        return {
+            "message": "No data found"
+        }
--- backend/mqtt.py
+++ backend/mqtt.py
@@ -1,4 +1,26 @@
 import paho.mqtt.client as mqtt
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+import time
+import random
 
-mqttc.connect("http://localhost/1883", 1883, 60)
+def init_mqtt():
+    # Define the MQTT client
+    # Use the appropriate MQTT version
+    # For example, if using MQTT v5.0, use mqtt.Client(mqtt.MQTTv5)
+    # For MQTT v3.1.1, use mqtt.Client()
+    # For MQTT v3.1, use mqtt.Client(mqtt.MQTTv311)
+    mqttc = mqtt.Client()
+    mqttc.username_pw_set("guest", "guest")
+    mqttc.connect("host.docker.internal", 1883, 60)
+
+    mqttc.loop_start()
+
+    print("Connected to MQTT broker")
+    while True:
+        # Publish a message to a topic
+        random_value = random.randint(0, 100)
+        print(f"Publishing random value: {random_value}")
+        mqttc.publish("sensors/temperature", str(random_value))
+
+        time.sleep(1)
+
+init_mqtt()
