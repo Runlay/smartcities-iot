45fb55a4d5a139190afc53b052daba0ecb27d60a
Runlay <runlaysnetwork@gmail.com>
2025-07-04 17:25:39 +0200

    deleted outdated files

--- planner/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# filepath: /Volumes/WD_Black SN770/Users/raycuement/GitHub Repositories/smartcities-iot/planner/entrypoint.sh
-
-# Run Fast Downward
-/fast-downward/fast-downward.py "$@"
-
-# Run your Python script
-pip install -r /requirements.txt
-python3 /mqtt_client.py
