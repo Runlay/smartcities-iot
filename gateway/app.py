9f5bd2a7a79bd0c1a60cbaaca4f2301d71998747
Finn Romaneessen <finn.romaneessen@tum.de>
2025-07-09 16:29:23 +0200

    Actuator update state

--- gateway/app.py
+++ gateway/app.py
@@ -13,51 +13,6 @@ mqtt_port = int(os.getenv("MQTT_PORT", "1883"))
 Device.pin_factory = NativeFactory()
 led = RGBLED(red=17, green=18, blue=19, pwm=False)
 bz = Buzzer(2)
-#button = Button(13) # TODO Pin
-
-topic_in = 'OpenZWave/1/#'
-topic_out = 'sensor/'
-
-def generate_json(sensor_type, value_key, value, unit):
-    return {
-        "type-id": f"de.uni-stuttgart.sciot.aeon/{sensor_type}",
-        "instance-id": f"0x{random.randint(100000000, 999999999):x}",
-        "timestamp": dt.now().isoformat() + "Z",
-        "value": {value_key: str(value)},
-    }
-
-def transform_message(client, userdata, msg):
-    payload = msg.payload.decode("utf-8")
-    data = json.loads(payload)
-
-    sensor_type = ''
-    value_key = ''
-    value = data['Value']
-
-    match data['Label']:
-        case 'Illuminance':
-            sensor_type = 'illuminance'
-            value_key = 'lux'
-        case 'Ultraviolet':
-            sensor_type = 'ultraviolet'
-            value_key = 'uv'
-        case 'Air Temperature':
-            sensor_type = 'temperature'
-            value_key = 'degrees'
-        case 'Humidity':
-            sensor_type = 'humidity'
-            value_key = 'percent'
-        case 'Home Security':
-            sensor_type = 'motion'
-            value_key = 'detected'
-            value = '1'
-        case _:
-            return
-
-    print(f'Message Received: {sensor_type}')
-
-    msg_out = generate_json(sensor_type, value_key, value, data['Units'])
-    client.publish(topic_out + sensor_type, json.dumps(msg_out))
 
 def connect_mqtt(topic, on_message):
     def on_connect(client, userdata, flags, rc, properties):
@@ -83,27 +38,32 @@ def light(client, userdata, msg):
     elif data['command'] == 'OFF':
         led.off()
 
+    state = {
+        "state": data['command']
+    }
+
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.publish('actuator/light/state', json.dumps(state))
+
 def alarm(client, userdata, msg):
     payload = msg.payload.decode("utf-8")
     data = json.loads(payload)
 
     if data['command'] == 'ON':
         bz.beep()
-        pass
     elif data['command'] == 'OFF':
         bz.off()
-        pass
 
-def button_press():
+    state = {
+        "state": data['command']
+    }
+
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    msg = generate_json('pressure', 'detected', '1', '')
-    client.publish(topic_out + 'pressure', json.dumps(msg))
+    client.publish('actuator/alarm/state', json.dumps(state))
 
 def main():
-    connect_mqtt(topic_in, transform_message)
     connect_mqtt('actuator/light/command', light)
     connect_mqtt('actuator/alarm/command', alarm)
-    #button.when_pressed = button_press
 
     while True:
         pass
